9ef44501e54a69ac2e16a2dbff92db1226378fd0
Konstantin <kost159@yandex.ru>
2017-04-01 17:58:33 +0300

    hma scrapper

---
+++ p_spider.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import scrapy
+import re
+
+class ProxySpider(scrapy.Spider):
+    name = 'proxy_spider'
+    start_urls = ['http://proxylist.hidemyass.com']
+
+    def parse(self, response):
+        id = 0
+        table_rows = response.css('.proxy-results tbody>tr')
+
+        for row in table_rows:
+
+            cls_none = row.css('td:nth-child(2)>span>style::text').re(r'\.(.*)\{display:none}')
+
+            ip_row = row.xpath("td[2]").re(r'</style>(.*)')[0]
+            port_row = row.xpath('td[3]//text()').extract()[0].lstrip()
+
+            for s in cls_none:
+                ip_row = re.sub(r'<span class="'+s+'">.*?</span>', '', ip_row)
+            ip_row = re.sub(r'<span style="display:none">.*?</span>', '', ip_row)
+            ip_row = re.sub(r'<div style="display:none">.*?</div>', '', ip_row)
+            ip_row = re.sub(r'</?span(.*?)>', '', ip_row)
+
+            yield {'id': id,
+                   'ip': ip_row.replace(' ',''),
+                   'port': port_row.replace(' ','') }
+
+            id += 1
